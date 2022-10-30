83d75b22d661b2357c90153217ca7abc9c8ca755
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-10-30 17:53:41 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -8,8 +8,7 @@ sleep 30s;
 target=$(docker-compose port answer 80)
 
 
-sed -i "s/http:/https:/g" ./answer-data/data/conf/config.yaml
-sed -i "s/127.0.0.1:9080/${DOMAIN}/g" ./answer-data/data/conf/config.yaml
+sed -i "s/'http://127.0.0.1:9080'/'https://${DOMAIN}'/g" ./answer-data/data/conf/config.yaml
 
 
 
