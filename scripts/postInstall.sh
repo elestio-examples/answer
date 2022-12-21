697deb91cb6114db60ad91b73b503526b9750b08
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-21 18:50:29 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,46 +1,36 @@
-#set env vars
-set -o allexport; source .env; set +o allexport;
+# #set env vars
+# set -o allexport; source .env; set +o allexport;
 
-#wait until the server is ready
-echo "Waiting for software to be ready ..."
-sleep 30s;
-
-target=$(docker-compose port answer 80)
-
-
-sed -i "s|http://127.0.0.1:9080|https://${DOMAIN}|g" ./answer-data/data/conf/config.yaml
-
-docker-compose down;
-docker-compose up -d;
+# #wait until the server is ready
 # echo "Waiting for software to be ready ..."
-sleep 30s;
+# sleep 30s;
+
+# target=$(docker-compose port answer 80)
 
 
-# First login
+# sed -i "s|http://127.0.0.1:9080|https://${DOMAIN}|g" ./answer-data/data/conf/config.yaml
 
-login=$(curl http://${target}/answer/api/v1/user/login/email \
-  -H 'accept: */*' \
-  -H 'accept-language: en_US' \
-  -H 'authorization;' \
-  -H 'content-type: application/json' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
-  --data-raw '{"e_mail":"admin@admin.com","pass":"admin"}' \
-  --compressed)
-
-  access_token=$(echo $login | jq -r '.data.access_token' )
+# docker-compose down;
+# docker-compose up -d;
+# # echo "Waiting for software to be ready ..."
+# sleep 30s;
 
 
-# Configure SMTP
-curl http://${target}/answer/admin/api/setting/smtp \
-  -X 'PUT' \
-  -H 'accept: */*' \
-  -H 'accept-language: en_US' \
-  -H 'authorization: '"${access_token}"'' \
-  -H 'content-type: application/json' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
-  --data-raw '{"from_email":"'"${DEFAULT_FROM_EMAIL}"'","from_name":"answer","smtp_host":"'"${EMAIL_HOST}"'","encryption":"","smtp_port":'"${EMAIL_PORT}"',"smtp_authentication":true,"smtp_username":"'"${EMAIL_HOST_USER}"'","smtp_password":"'"${EMAIL_HOST_PASSWORD}"'","test_email_recipient":""}' \
-  --compressed
+# # First login
 
+# login=$(curl http://${target}/answer/api/v1/user/login/email \
+#   -H 'accept: */*' \
+#   -H 'accept-language: en_US' \
+#   -H 'authorization;' \
+#   -H 'content-type: application/json' \
+#   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
+#   --data-raw '{"e_mail":"admin@admin.com","pass":"admin"}' \
+#   --compressed)
+
+#   access_token=$(echo $login | jq -r '.data.access_token' )
+
+
+# # Configure SMTP
 # curl http://${target}/answer/admin/api/setting/smtp \
 #   -X 'PUT' \
 #   -H 'accept: */*' \
@@ -48,33 +38,43 @@ curl http://${target}/answer/admin/api/setting/smtp \
 #   -H 'authorization: '"${access_token}"'' \
 #   -H 'content-type: application/json' \
 #   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
-#   --data-raw '{"from_email":"answer@vm.elestio.app","from_name":"answer","smtp_host":"172.17.0.1","encryption":"","smtp_port":25,"smtp_authentication":true,"smtp_username":"","smtp_password":"","test_email_recipient":""}' \
+#   --data-raw '{"from_email":"'"${DEFAULT_FROM_EMAIL}"'","from_name":"answer","smtp_host":"'"${EMAIL_HOST}"'","encryption":"","smtp_port":'"${EMAIL_PORT}"',"smtp_authentication":true,"smtp_username":"'"${EMAIL_HOST_USER}"'","smtp_password":"'"${EMAIL_HOST_PASSWORD}"'","test_email_recipient":""}' \
+#   --compressed
+
+# # curl http://${target}/answer/admin/api/setting/smtp \
+# #   -X 'PUT' \
+# #   -H 'accept: */*' \
+# #   -H 'accept-language: en_US' \
+# #   -H 'authorization: '"${access_token}"'' \
+# #   -H 'content-type: application/json' \
+# #   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
+# #   --data-raw '{"from_email":"answer@vm.elestio.app","from_name":"answer","smtp_host":"172.17.0.1","encryption":"","smtp_port":25,"smtp_authentication":true,"smtp_username":"","smtp_password":"","test_email_recipient":""}' \
+# #   --compressed
+
+
+# #   changing email addresse
+
+# curl http://${target}/answer/api/v1/user/email/change/code \
+#   -H 'accept: */*' \
+#   -H 'accept-language: en_US' \
+#   -H 'authorization: '"${access_token}"'' \
+#   -H 'content-type: application/json' \
+#   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
+#   --data-raw '{"e_mail":"'"${ADMIN_EMAIL}"'"}' \
 #   --compressed
 
 
-#   changing email addresse
+# #   Changing passwords
 
-curl http://${target}/answer/api/v1/user/email/change/code \
-  -H 'accept: */*' \
-  -H 'accept-language: en_US' \
-  -H 'authorization: '"${access_token}"'' \
-  -H 'content-type: application/json' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
-  --data-raw '{"e_mail":"'"${ADMIN_EMAIL}"'"}' \
-  --compressed
-
-
-#   Changing passwords
-
-curl http://${target}/answer/api/v1/user/password \
-  -X 'PUT' \
-  -H 'accept: */*' \
-  -H 'accept-language: en_US' \
-  -H 'authorization: '"${access_token}"'' \
-  -H 'content-type: application/json' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
-  --data-raw '{"old_pass":"admin","pass":"'"${ADMIN_PASSWORD}"'"}' \
-  --compressed
+# curl http://${target}/answer/api/v1/user/password \
+#   -X 'PUT' \
+#   -H 'accept: */*' \
+#   -H 'accept-language: en_US' \
+#   -H 'authorization: '"${access_token}"'' \
+#   -H 'content-type: application/json' \
+#   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
+#   --data-raw '{"old_pass":"admin","pass":"'"${ADMIN_PASSWORD}"'"}' \
+#   --compressed
 
 
 
